06f30b88911aad1c224d89e8ec1de27191464ba1
islandmakerepic <mysandwich00@gmail.com>
2016-03-20 21:19:37 -0700

    Update burnInHell.lua

--- burnInHell.lua
+++ burnInHell.lua
@@ -2,27 +2,32 @@ local Player = game.Players.LocalPlayer
 local Character = Player.Character
 local Mouse = Player:GetMouse()
 
+local GetRandom=function()
+  return math.random(-360,360)
+  end
+
 local Fly = function(Direction)
 local Distance = (Character.Torso.CFrame.Y-workspace.Base.CFrame.Y)
 Distance = Distance - 2.5
   local woah = Instance.new("Part",workspace)
-  woah.Size = Vector3.new(15,Distance,15)
+  woah.Size = Vector3.new(15,Distance,20)
   woah.BrickColor = workspace.Base.BrickColor
   woah.Material = 'Grass'
   woah.Anchored = true
   woah.CFrame = Character.Torso.CFrame
   woah.CFrame = woah.CFrame* CFrame.new(0,-(Distance/2),0)* CFrame.new(0,-2.5,0)
-    game.Debris:AddItem(woah,0.4)
+    game.Debris:AddItem(woah,1)
 
   local crap = Instance.new("Part",workspace)
   crap.CanCollide = false
   crap.Anchored = true
-  crap.Shape = 'Ball'
   crap.Size = Vector3.new(10,10,10)
   crap.BrickColor = BrickColor.White()
   crap.Material = 'Neon'
-  crap.CFrame = Character.Torso.CFrame*CFrame.new(0,0,20)
-    game.Debris:AddItem(crap,0.7)
+crap.CFrame = Character.Torso.CFrame
+*CFrame.new(0,0,40)
+*CFrame.Angles(math.rad(GetRandom()),math.rad(GetRandom()),math.rad(GetRandom()))
+    game.Debris:AddItem(crap,1.25)
       Character.Torso.CFrame = CFrame.new(Character.Torso.CFrame.p,Direction.p)*CFrame.new(0,0,-2.5)
 end
 
