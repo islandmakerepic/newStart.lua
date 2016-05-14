9b1aa513677289d7053035b7b5dfb7d3717350f1
islandmakerepic <mysandwich00@gmail.com>
2016-05-14 14:36:52 -0700

    Update attempt.lua

--- attempt.lua
+++ attempt.lua
@@ -4,7 +4,7 @@ ORB.Name=n
 ORB.Shape='Ball'
 ORB.Size=Vector3.new(1,1,1)
 ORB.Anchored=true
-ORB.CFrame=char.Torso.CFrame*CFrame.new(2+(math.random(0,1)==1 and n or -n),-1,-(5+n))
+ORB.CFrame=char.Torso.CFrame*CFrame.new(2+n-math.random(-1,3),-1,-(5+n))
 ORB.Transparency=0.5
 ORB.BrickColor=BrickColor.Random()
 return ORB
