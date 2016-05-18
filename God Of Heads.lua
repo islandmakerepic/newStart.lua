3981302d274091f637395696aaab3c53f9c3941a
islandmakerepic <mysandwich00@gmail.com>
2016-05-17 17:53:26 -0700

    Update God Of Heads.lua

--- God Of Heads.lua
+++ God Of Heads.lua
@@ -31,13 +31,46 @@ local attacks = {
 return
 end,
 ['devour']=function(position,target)
-  
-for i=-25,25,5 do
+  coroutine.wrap(function()
+    for i=-25,25,5 do
   if math.random(0,3)==1 then wait(0) end
 local attack=headMain:clone()
 attack.Anchored=true
 attack.Parent=char
 attack.CFrame=char.Torso.CFrame*CFrame.new(i,-3,math.random(-15,15))
+coroutine.wrap(function()
+  for i=0,300,50 do
+    wait(0)
+    attack.CFrame=attack.CFrame*CFrame.new(0,0.50,0)
+    end
+  end)()
+attack.Material='Neon'
+attack:breakJoints()
+
+Instance.new("PointLight",attack)
+local diff=(position-attack.CFrame.p).magnitude
+--print(diff)
+game.Debris:AddItem(attack,(diff/65)+1.5)
+coroutine.wrap(function()
+  wait((diff/65)+1.48)
+  Kill(attack.Position)
+  end)()
+coroutine.wrap(function()
+  wait(1)
+  Instance.new("Fire",attack).Color=BrickColor.new'Toothpaste'.Color
+  while attack.Parent==char and wait(0) do
+    cf=CFrame.new(position)
+attack.CFrame=CFrame.new(attack.CFrame.p,(cf*CFrame.new(i/5,0,0)).p)*CFrame.new(i<0 and math.random(0,5)/10 or math.random(-5,0)/10,0,-2)
+    end
+  end)()
+end
+end)()
+for i=-25,25,5 do
+  if math.random(0,3)==1 then wait(0) end
+local attack=headMain:clone()
+attack.Anchored=true
+attack.Parent=char
+attack.CFrame=char.Torso.CFrame*CFrame.new(i,-3,math.random(-30,15))
 coroutine.wrap(function()
   for i=0,300,50 do
     wait(0)
