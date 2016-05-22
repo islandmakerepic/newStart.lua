276d8e28b4b160df46a2162dbd1d3d0cccbe45e4
islandmakerepic <mysandwich00@gmail.com>
2016-05-21 22:38:35 -0700

    Update God Of Heads.lua

--- God Of Heads.lua
+++ God Of Heads.lua
@@ -101,7 +101,7 @@ coroutine.wrap(function()
   end)()
 coroutine.wrap(function()
   wait(1)
-  Instance.new("Fire",attack).Color=BrickColor.new'Toothpaste'.Color
+ 
   attack.Touched:connect(function(h)
     if h.Name=='Head' or h.Parent==char then return end
     pcall(function() h.Parent.Humanoid.Health=h.Parent.Humanoid.Health-20 end)
