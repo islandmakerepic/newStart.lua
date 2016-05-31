9481950df9502dfb1e99516ea8b1e14500ca36c6
islandmakerepic <mysandwich00@gmail.com>
2016-05-30 19:40:06 -0700

    Update OhDamn.lua

--- OhDamn.lua
+++ OhDamn.lua
@@ -4,5 +4,5 @@ cyl.FormFactor='Custom'
 cyl.Size=Vector3.new(1,5,5)
 
 cyl.Parent=workspace
-cyl.CFrame=owner.Character.Torso.CFrame*CFrame.new(0,0,-5)
+cyl.CFrame=owner.Character.Torso.CFrame*CFrame.new(0,0,-5)*CFrame.Angles(0,math.rad(90),0)
 cyl.Anchored=true
