7fa7889bd402b721a77299b1b209c7a1337e6123
islandmakerepic <mysandwich00@gmail.com>
2016-05-30 19:39:44 -0700

    Update OhDamn.lua

--- OhDamn.lua
+++ OhDamn.lua
@@ -1,7 +1,7 @@
 local cyl=Instance.new("Part")
 cyl.Shape='Cylinder'
 cyl.FormFactor='Custom'
-cyl.Size=Vector3.new(5,1,5)
+cyl.Size=Vector3.new(1,5,5)
 
 cyl.Parent=workspace
 cyl.CFrame=owner.Character.Torso.CFrame*CFrame.new(0,0,-5)
