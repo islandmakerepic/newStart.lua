896281f970d16a2206cdaef72e33e88ce3b1c881
islandmakerepic <mysandwich00@gmail.com>
2016-05-30 20:25:34 -0700

    Update OhDamn.lua

--- OhDamn.lua
+++ OhDamn.lua
@@ -5,22 +5,36 @@ cyl.Size=Vector3.new(0.5,2.5,2.5)
 local owner=script.Parent
 local plr=game.Players[owner.Name]
 local cyls={}
-local arm=owner['Right Arm']
-local W=Instance.new("Weld",arm)
-W.Part0=arm
-W.Part1=owner.Torso
-W.C0=CFrame.new(-1.5,-0.5,0)*CFrame.Angles(math.rad(90),math.rad(27),0)*CFrame.new(0,0,-1.5)
+local arm=owner['Right Arm']:clone()
+arm.Parent=owner
+owner['Right Arm']:Destroy()
+arm.Size=Vector3.new(1,1,2)
+arm.Anchored=true
+arm:breakJoints()
+local arm2=owner['Left Arm']:clone()
+arm2.Parent=owner
+arm2.Size=Vector3.new(1,1,2)
+arm2.Anchored=true
+arm2:breakJoints()
+owner['Left Arm']:Destroy()
+local hrp=owner['HumanoidRootPart']
+local b=owner.Torso.CFrame
 for i=-80,80,20 do
-  local c=cyl:clone()
+hrp.CFrame=b*CFrame.Angles(0,0,math.rad(90))
+local c=cyl:clone()
 c.Parent=workspace
-c.CFrame=owner.Torso.CFrame*CFrame.Angles(0,math.rad(i),0)*CFrame.new(0,4,-10)*CFrame.Angles(0,math.rad(90),0)
+c.CFrame=owner.Torso.CFrame*CFrame.Angles(0,math.rad(i),0)*CFrame.new(0,0,-10)*CFrame.Angles(0,math.rad(90),0)
 c.Anchored=true
+arm.CFrame=CFrame.new((owner.Torso.CFrame*CFrame.new(1.3,0,0)).p,(c.CFrame*CFrame.new(1,0,0)).p)*CFrame.new(0,0,-1)
 cyls[#cyls+1]=c
 wait(0.1)
 end
+
 wait(0.5)
+
 for i=#cyls,1,-1 do
   wait(0.1)
 local ok=cyls[i]
 ok.Material='Neon'
+arm2.CFrame=CFrame.new( (owner.Torso.CFrame*CFrame.new(-1.4,0,0)).p , (ok.CFrame*CFrame.new(-1,0,0)).p )*CFrame.new(0,0,-1)
 end
