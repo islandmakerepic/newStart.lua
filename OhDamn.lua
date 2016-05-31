c2a4dd6aba2a1db79d8e82803ddf4c4358eff269
islandmakerepic <mysandwich00@gmail.com>
2016-05-30 19:54:38 -0700

    Update OhDamn.lua

--- OhDamn.lua
+++ OhDamn.lua
@@ -1,14 +1,26 @@
 local cyl=Instance.new("Part")
 cyl.Shape='Cylinder'
 cyl.FormFactor='Custom'
-cyl.Size=Vector3.new(1,3,3)
+cyl.Size=Vector3.new(0.5,2.5,2.5)
 local owner=script.Parent
 local plr=game.Players[owner.Name]
-
-for i=-90,90,30 do
+local cyls={}
+local arm=owner['Right Arm']
+local W=Instance.new("Weld",arm)
+W.Part0=arm
+W.Part1=owner.Torso
+W.C0=CFrame.new(1.5,0,0)
+for i=-80,80,20 do
   local c=cyl:clone()
 c.Parent=workspace
-c.CFrame=owner.Torso.CFrame*CFrame.Angles(0,math.rad(i),0)*CFrame.new(0,4,-6)*CFrame.Angles(0,math.rad(90),0)
+c.CFrame=owner.Torso.CFrame*CFrame.Angles(0,math.rad(i),0)*CFrame.new(0,4,-10)*CFrame.Angles(0,math.rad(90),0)
 c.Anchored=true
-wait()
+cyls[#cyls+1]=c
+wait(0.1)
+end
+wait(0.5)
+for i=#cyls,1,-1 do
+  wait(0.1)
+local ok=cyls[i]
+ok.Material='Neon'
 end
