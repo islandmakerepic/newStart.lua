0bebf293f60504d13d81a20efcc848073805e56c
islandmakerepic <mysandwich00@gmail.com>
2016-05-30 19:44:59 -0700

    Update OhDamn.lua

--- OhDamn.lua
+++ OhDamn.lua
@@ -5,7 +5,7 @@ cyl.Size=Vector3.new(1,3,3)
 local owner=script.Parent
 local plr=game.Players[owner.Name]
 
-for i=180,360,30 do
+for i=90,180,30 do
   local c=cyl:clone()
 c.Parent=workspace
 c.CFrame=owner.Torso.CFrame*CFrame.Angles(0,math.rad(i),0)*CFrame.new(0,4,-6)*CFrame.Angles(0,math.rad(90),0)
