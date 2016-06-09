1fc5099cf8ae745df2e6e09cffae697b2f79cf57
islandmakerepic <mysandwich00@gmail.com>
2016-06-08 17:17:52 -0700

    Create WellThen.lua

---
+++ WellThen.lua
@@ -0,0 +1,22 @@
+--cylinders, spikes
+--possibly wings
+
+local plr = game.Players.LocalPlayer
+local char = plr.Character
+local arm = char['Right Arm']
+arm.Transparency = 1
+local Arm = arm:clone()
+Arm.Transparency=0
+Arm.Size=Vector3.new(Arm.Size.X,Arm.Size.Y/2,Arm.Size.Z)
+local Weld=Instance.new("Weld",Arm)
+Weld.Part0=Arm
+Weld.Part1=char.Head
+Weld.C0=CFrame.new(-1.6,0.675,0)*CFrame.Angles(0,0,math.rad(20))
+local Arm2=Arm:clone()
+Arm2.Parent=char
+Arm2.Name='Part2'
+local Weld2=Instance.new("Weld",Arm2)
+Weld2.Part0=Arm2
+Weld2.Part1=Arm
+Weld2.C0=CFrame.new(0,Arm.Size.Y,0)
+local c=Instance.new("Part",workspace)
