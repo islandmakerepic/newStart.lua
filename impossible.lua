5bba20d97e8144cb9b3a23d2929cdad405e26c79
islandmakerepic <mysandwich00@gmail.com>
2016-12-22 01:04:45 -0700

    Update impossible.lua

--- impossible.lua
+++ impossible.lua
@@ -15,6 +15,7 @@ local clones={}
 m.Button1Down:connect(function()
 if on then for i,v in pairs(clones) do v:Destroy() end return end
 on=true
+
 for i=-25,25,7.5 do
 local nc=dupe:clone()
 nc.Parent=workspace
@@ -36,4 +37,5 @@ end
       local z=i%2==0 and -i or -i+(math.random(-1,1)*5)
       clones[i].Torso.CFrame=clones[i].Torso.CFrame*CFrame.new(0,0,z)
       end
+end
 end)
