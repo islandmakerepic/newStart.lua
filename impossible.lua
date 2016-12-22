dd7c34706016f139f9cc9e3af936d7fcbacb8dce
islandmakerepic <mysandwich00@gmail.com>
2016-12-22 01:02:34 -0700

    Update impossible.lua

--- impossible.lua
+++ impossible.lua
@@ -1,6 +1,7 @@
 local plr=game.Players.LocalPlayer
 local cyl=Instance.new("Part")
 cyl.Size=Vector3.new(4,0.2,4)
+cyl.Transparency=0.5
 Instance.new("CylinderMesh",cyl)
 on=false
 
@@ -21,7 +22,16 @@ nc.Parent=workspace
 nc:MakeJoints()
 nc.Torso.CFrame=char.Torso.CFrame*CFrame.new(i,0,0)
 clones[#clones+1]=nc
+                                    
+for i=1,5 do
+ local CLL=cyl:clone()
+CLL.Anchored=true
+CLL.CanCollide=false
+  CLL.CFrame=nc.Torso.CFrame*CFrame.new(0,-1.5+i)
+game.Debris:AddItem(CLL,0.3)
+                                                wait(0)
 end
+
     for i=1,#clones do
       local z=i%2==0 and -i or -i+(math.random(-1,1)*5)
       clones[i].Torso.CFrame=clones[i].Torso.CFrame*CFrame.new(0,0,z)
