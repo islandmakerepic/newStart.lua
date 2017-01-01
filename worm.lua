76f6b8413f39e359b934f046710ce0e38c51efaa
islandmakerepic <mysandwich00@gmail.com>
2017-01-01 14:08:48 -0700

    Update worm.lua

--- worm.lua
+++ worm.lua
@@ -16,9 +16,9 @@ local mou=plr:GetMouse()
 mou.KeyDown:connect(function(K)
 if K=='t' then
 change()
-if not incam then
+if incam==false then
 plr.Character.Torso.CFrame=Torso.CFrame
-else plr.Character.Torso.CFrame=plr.Character.Torso.CFrame.new(0,1000,0)
+elseif incam==true then plr.Character.Torso.CFrame=plr.Character.Torso.CFrame.new(0,1000,0)
 end
 end
 end)
