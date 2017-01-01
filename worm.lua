dd3f40f09a07d7fd939a2b6aada72849055e94e7
islandmakerepic <mysandwich00@gmail.com>
2017-01-01 14:22:01 -0700

    Update worm.lua

--- worm.lua
+++ worm.lua
@@ -19,7 +19,7 @@ change()
                         
 if incam==false then
 plr.Character.Parent=workspace
-        local CF=Torso.CFrame
+        local CF=plr.Character.Torso.CFrame*CFrame.new(0,-1000,0)
 plr.Character.Torso.CFrame=CF
 elseif incam==true then plr.Character.Parent=workspace.CurrentCamera
 plr.Character.Torso.CFrame=plr.Character.Torso.CFrame*CFrame.new(0,1000,0)
@@ -37,7 +37,7 @@ Torso.CanCollide=false
 Torso.Transparency=1
 newbase.CFrame=workspace.Base.CFrame*CFrame.new(0,1000,0)
 Torso.Anchored=true
-local Mat,Colorr='Plastic','Really brown'
+local Mat,Colorr='Grass','Really brown'
 
 local r=function() return math.random(-360,360) end
 local camlook=workspace.CurrentCamera.CameraSubject
@@ -55,12 +55,12 @@ end
 if incam then
 
 local Block=Instance.new("Part",workspace.Base)
-Block.Size=Vector3.new(3,3,3)
+Block.Size=Vector3.new(4,4,4)
 Block.Anchored=true
 Block.Material=Mat
 Block.BrickColor=BrickColor.new(Colorr)
     Torso.CFrame=plr.Character.Torso.CFrame*CFrame.new(0,-1000,0)
-Block.CFrame=Torso.CFrame*CFrame.new(0,-4,0)*CFrame.Angles(math.rad(r()),math.rad(r()),math.rad(r()))
+Block.CFrame=Torso.CFrame*CFrame.new(math.random(-16,13)/10,-4.2,0)*CFrame.Angles(math.rad(r()),math.rad(r()),math.rad(r()))
 game.Debris:AddItem(Block,1.1)
     workspace.CurrentCamera.CameraSubject=Torso
     else
