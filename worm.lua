b74b6720058b4e43c94e2029a9ca122d3629dc25
islandmakerepic <mysandwich00@gmail.com>
2017-01-01 14:15:25 -0700

    Update worm.lua

--- worm.lua
+++ worm.lua
@@ -16,31 +16,40 @@ local mou=plr:GetMouse()
 mou.KeyDown:connect(function(K)
 if K=='t' then
 change()
+                        
 if incam==false then
+plr.Character.Parent=workspace
 plr.Character.Torso.CFrame=Torso.CFrame
-elseif incam==true then plr.Character.Torso.CFrame=plr.Character.Torso.CFrame*CFrame.new(0,1000,0)
+elseif incam==true then plr.Character.Parent=workspace.CurrentCamera
+plr.Character.Torso.CFrame=plr.Character.Torso.CFrame*CFrame.new(0,1000,0)
 end
+
 end
 end)
+
 local Torso=plr.Character.Torso:clone()
 local newbase=workspace.Base:clone()
 newbase.Parent=workspace.CurrentCamera
 Torso.Parent=workspace.CurrentCamera
 Torso:breakJoints()
+Torso.CanCollide=false
+Torso.Transparency=1
 newbase.CFrame=workspace.Base.CFrame*CFrame.new(0,1000,0)
 Torso.Anchored=true
 local Mat,Colorr='Plastic','Really brown'
 
 local r=function() return math.random(-360,360) end
 local camlook=workspace.CurrentCamera.CameraSubject
-while true do
-local ic=incam
-wait(0)
-if ic~=incam then
-plr.Character.Parent=incam and workspace.CurrentCamera or workspace
-plr.Character.Humanoid.Jump=true
+while wait(0) do
 plr.Character.Humanoid.WalkSpeed=incam and 32 or 16
-        plr.Character.Humanoid.JumpPower=40
+plr.Character.Parent=incam and workspace.CurrentCamera or workspace
+        
+if incam then
+plr.Character.Humanoid.JumpPower=100
+else
+plr.Character.Humanoid.JumpPower=30
+end
+
 end
 
 if incam then
