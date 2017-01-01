4225b985788d3598640d4032d2148ce45cd54182
islandmakerepic <mysandwich00@gmail.com>
2017-01-01 14:52:22 -0700

    Update worm.lua

--- worm.lua
+++ worm.lua
@@ -87,7 +87,7 @@ Block.BrickColor=BrickColor.new(Colorr)
     local cl=Block:Clone()
     cl.Parent=Block
     cl.Size=Vector3.new(3,3,NUM)
-    cl.CFrame=CFrame.new(Torso.Position,Block.Position)*CFrame.new(0,0,-NUM)
+    cl.CFrame=CFrame.new((Torso.CFrame*CFrame.new(0,0,-1)).p,Block.Position)*CFrame.new(0,0,-NUM)
     Torso.CFrame=plr.Character.Torso.CFrame*CFrame.new(0,-1000,0)
 Block.CFrame=Torso.CFrame*CFrame.new(math.random(-16,13)/10,-4.2,0)*CFrame.Angles(math.rad(r()),math.rad(r()),math.rad(r()))
 game.Debris:AddItem(Block,1.1)
